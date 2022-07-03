296ead9ddbac66981cbe6a906fa75886b2d58ba2
ashkan semsar <ashkansemsar@hotmail.com>
2022-07-03 09:12:26 +0430

    AddLeaf

---
+++ CompositeDesignPattern/Infra/Leaf.cs
@@ -0,0 +1,25 @@
+﻿using CompositeDesignPattern.Interface;
+
+namespace CompositeDesignPattern.Infra;
+
+public class Leaf:ICustomComponent
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public Leaf(string name,decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public void AddComponent(ICustomComponent component)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal CalculatePrice()
+    {
+        return Price;
+    }
+}
