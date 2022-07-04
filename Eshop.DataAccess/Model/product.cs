110ee3a98949aa96bbf5beb5abf9dc9ed05fdd80
ashkan semsar <ashkansemsar@hotmail.com>
2022-07-04 10:36:29 +0430

    Add eshop_dataAccess and product model

---
+++ Eshop.DataAccess/Model/product.cs
@@ -0,0 +1,21 @@
+﻿namespace Eshop_DataAccess.Model
+{
+    public class product
+    {
+        public string ArticleId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public product()
+        {
+            
+        }
+
+        public product(string articleId,string name,decimal price)
+        {
+            ArticleId=articleId;
+            Name=name; 
+            Price=price;
+        }
+    }
+}
